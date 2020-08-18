1e968e9d2aae3d305efb5283fbfd1c525afeec96
mengmeng19 <mengmeng19@staff.weibo.com>
2020-08-18 16:32:07 +0800

    微前端-qiankun-demo

--- qiankun-tutorial/vue-student/src/main.js
+++ qiankun-tutorial/vue-student/src/main.js
@@ -29,16 +29,34 @@ if (!window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
 
+/*
+* 微应用需要在自己的入口 js (通常就是你配置的 webpack 的 entry js) 导出 bootstrap、mount、unmount 三个生命周期钩子，以供主应用在适当的时机调用。
+* */
+/**
+ * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
+ * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
+ */
 export async function bootstrap() {
   console.log('student app bootstraped');
 }
-
+/**
+ * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
+ */
 export async function mount(props) {
   console.log('student app mount', props);
   render(props)
 }
+/**
+ * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
+ */
 
 export async function unmount() {
   console.log('student app unmount');
   vm.$destroy()
+}
+/**
+ * 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
+ */
+export async function update(props) {
+  console.log('student app update', props);
 }
