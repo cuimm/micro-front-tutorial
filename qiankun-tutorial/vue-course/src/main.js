1bec02bb531aa4f5db639ab1770b5809cae69c02
mengmeng19 <mengmeng19@staff.weibo.com>
2020-08-18 16:33:21 +0800

    微前端-qiankun-demo

--- qiankun-tutorial/vue-course/src/main.js
+++ qiankun-tutorial/vue-course/src/main.js
@@ -34,23 +34,20 @@ if (!window.__POWERED_BY_QIANKUN__) {
 export async function bootstrap() {
   console.log('course app bootstrap');
 }
-/**
- * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
- */
+
+// 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
 export async function mount(props) {
   console.log('course app mount');
   render()
 }
-/**
- * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
- */
+
+// 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
 export async function unmount() {
   console.log('course app unmount');
   vm.$destroy()
 }
-/**
- * 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
- */
+
+// 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
 export async function update(props) {
   console.log('course app update', props);
 }
--- qiankun-tutorial/vue-student/src/main.js
+++ qiankun-tutorial/vue-student/src/main.js
@@ -39,24 +39,20 @@ if (!window.__POWERED_BY_QIANKUN__) {
 export async function bootstrap() {
   console.log('student app bootstraped');
 }
-/**
- * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
- */
+
+// 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
 export async function mount(props) {
   console.log('student app mount', props);
   render(props)
 }
-/**
- * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
- */
 
+// 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
 export async function unmount() {
   console.log('student app unmount');
   vm.$destroy()
 }
-/**
- * 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
- */
+
+// 可选生命周期钩子，仅使用 loadMicroApp 方式加载微应用时生效
 export async function update(props) {
   console.log('student app update', props);
 }
