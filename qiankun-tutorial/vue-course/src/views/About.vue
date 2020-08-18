a42aa94d66131337621e9cf1ce2bea79d3181ba3
mengmeng19 <mengmeng19@staff.weibo.com>
2020-08-18 16:29:26 +0800

    微前端-qiankun-demo

--- qiankun-tutorial/vue-course/src/views/About.vue
+++ qiankun-tutorial/vue-course/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>20000</h1>
+    <h1>Course子项目</h1>
     <h1>This is an about page</h1>
   </div>
 </template>
--- qiankun-tutorial/vue-student/src/views/About.vue
+++ qiankun-tutorial/vue-student/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>10000</h1>
+    <h1>Student子项目</h1>
     <h1>This is an about page</h1>
   </div>
 </template>
